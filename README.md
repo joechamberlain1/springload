9788cbc0538b65fd9e3c80c0036fffa3ba1c5701
joechamberlain1 <joechamberlain2801@gmail.com>
2021-07-22 13:26:17 +1200

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+#Springload Form Validation Problem
+
+Please run npm install after cloning the repo to install the packages for react icons and react hook form.
+
+I used React Hook Form as it's something that I've used in the past and requires less code and removes unneccasry re-rendering.
+
+So to start with I made the bare bones of the form. I added the title, input fields and labels for email address and password. I then apply the necassary values to the inputs so the react hook form can add these to the data. React hook form also allows me to add errors to each input.
+
+When picking the best method for the colour choice I decided to with the <select> attribute as it looked considerably neater than radio. The radio switches above the checkboxes looked quite messy and confusing.
+
+Checkboxes were the best choice for the animal selection part as it allowed for multiple choices.
+
+I kept the styling relatively simple as I spent more time on functionality and making sure the form was collecting the right data.
+
+One thing I struggled on trying to figure out was how to close the input bar for the favourite type of tiger. I did spend a large chunk of time on this and I will definitely continue to try and solve the problem after this has been sent off.
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,129 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  GiBearFace,
+  GiDonkey,
+  GiRattlesnake,
+  GiTigerHead,
+} from "react-icons/gi";
+import "./App.css";
 
 function App() {
+  const [tiger, setTiger] = useState(false);
+  const showTiger = () => setTiger(true);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+    alert("Thanks for registering, please check your details in the console!");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form">
+        <div className="header">
+          <h1>Contact Form</h1>
+        </div>
+
+        {/* email and password section starts here */}
+
+        <div className="email">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="input">
+              <label htmlFor="">Email: </label>
+              <input
+                type="email"
+                {...register("Email", { required: true })}
+                placeholder="Please enter a valid email address"
+              />
+              {errors.Email?.type === "required" && "Email is required"}
+              <label htmlFor="">Password: </label>
+              <input
+                type="password"
+                {...register("password", { required: true, minLength: 8 })}
+                placeholder="Please enter a password"
+                minLength="8"
+              />
+              {errors.password?.type === "required" && "Password is required"}
+            </div>
+
+            {/* colour section starts here */}
+
+            <div className="colors">
+              <h2>What's your favourtie colour?</h2>
+              <select name="colors" {...register("Color")}>
+                <option value="Blue">Blue</option>
+                <option value="Green">Green</option>
+                <option value="Red">Red</option>
+                <option value="Black">Black</option>
+                <option value="Brown">Brown</option>
+              </select>
+            </div>
+
+            {/* animal section starts here */}
+
+            <div className="animals">
+              <h2>What's your favourtie Animal?</h2>
+              <div className="animal">
+                <input
+                  type="checkbox"
+                  name="Animal"
+                  value="Bear"
+                  {...register("Animal")}
+                />
+
+                <label htmlFor="Bear">Bear</label>
+                <GiBearFace />
+              </div>
+
+              <div className="animal">
+                <input
+                  onClick={showTiger}
+                  type="checkbox"
+                  name="Animal"
+                  value="Tiger"
+                  {...register("Animal")}
+                />
+                <label htmlFor="Tiger">Tiger </label> <GiTigerHead />
+              </div>
+              <div className="tiger">
+                {tiger ? (
+                  <input
+                    type="text"
+                    placeholder="What's your favourite type of tiger?"
+                    {...register("Tiger")}
+                  />
+                ) : null}
+              </div>
+              <div className="animal">
+                <input
+                  type="checkbox"
+                  name="Animal"
+                  value="Snake"
+                  {...register("Animal")}
+                />
+                <label htmlFor="Snake">Snake</label> <GiRattlesnake />
+              </div>
+              <div className="animal">
+                <input
+                  type="checkbox"
+                  name="Animal"
+                  value="Donkey"
+                  {...register("Animal")}
+                />
+                <label htmlFor="Donkey">Donkey</label> <GiDonkey />
+              </div>
+            </div>
+            <button type="submit" value="Submit">
+              {" "}
+              Submit{" "}
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
